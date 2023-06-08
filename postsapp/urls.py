a29914c60518cb90a632bb3bb2dc983aaef01aa6
Pavel <sdpv55@gmail.com>
2023-06-08 22:12:47 +0300

    Moved the search filter to a separate page, because filters and pagination on the same page do not work

--- postsapp/urls.py
+++ postsapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostsList, PostsDetailView, PostsCreateView
+from .views import PostsList, PostsDetailView, PostsCreateView, PostsSearchView
 
 urlpatterns = [
     path('', PostsList.as_view()),
     path('<int:pk>/', PostsDetailView.as_view(), name='post'),
+    path('search/', PostsSearchView.as_view(), name='search'),
     path('create/', PostsCreateView.as_view(), name='create'),
 ]
