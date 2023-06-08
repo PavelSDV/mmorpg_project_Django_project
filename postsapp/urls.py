7fecaf64029f8b184dcdd41c75a13fce0770535b
Pavel <sdpv55@gmail.com>
2023-06-08 23:35:09 +0300

    Add Update, Delite post

--- postsapp/urls.py
+++ postsapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PostsList, PostsDetailView, PostsCreateView, PostsSearchView
+from .views import PostsList, PostsDetailView, PostsCreateView, PostsSearchView, PostsUpdateView, PostsDeleteView
 
 urlpatterns = [
     path('', PostsList.as_view()),
     path('<int:pk>/', PostsDetailView.as_view(), name='post'),
     path('search/', PostsSearchView.as_view(), name='search'),
     path('create/', PostsCreateView.as_view(), name='create'),
+    path('create/<int:pk>/', PostsUpdateView.as_view(), name='create'),
+    path('delete/<int:pk>/', PostsDeleteView.as_view(), name='delete'),
 ]
