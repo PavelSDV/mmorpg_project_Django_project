02fbee50fc802c1c0af65f9b03624cec341ca6f3
Pavel <sdpv55@gmail.com>
2023-06-18 20:47:42 +0300

    Add user may accept or delete response

--- postsapp/urls.py
+++ postsapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import PostsList, PostsDetailView, PostsCreateView, \
-    PostsSearchView, PostsUpdateView, PostsDeleteView, ProfileView
+    PostsSearchView, PostsUpdateView, PostsDeleteView, ProfileView, \
+    accept_response, delete_response
 
 
 urlpatterns = [
@@ -11,4 +12,7 @@ urlpatterns = [
     path('create/<int:pk>/', PostsUpdateView.as_view(), name='create'),
     path('delete/<int:pk>/', PostsDeleteView.as_view(), name='delete'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('accept_response/<int:pk>/', accept_response, name='accept_response'),
+    path('delete_response/<int:pk>/', delete_response, name='delete_response'),
+
 ]
