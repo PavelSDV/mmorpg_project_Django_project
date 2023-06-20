328d24c613a9405b37ac41bab83c57560b643134
Pavel <sdpv55@gmail.com>
2023-06-20 22:04:19 +0300

    Add menu Bootstrap

--- postsapp/urls.py
+++ postsapp/urls.py
@@ -5,7 +5,7 @@ from .views import PostsList, PostsDetailView, PostsCreateView, \
 
 
 urlpatterns = [
-    path('', PostsList.as_view()),
+    path('', PostsList.as_view(), name='posts'),
     path('<int:pk>/', PostsDetailView.as_view(), name='post'),
     path('search/', PostsSearchView.as_view(), name='search'),
     path('create/', PostsCreateView.as_view(), name='create'),
